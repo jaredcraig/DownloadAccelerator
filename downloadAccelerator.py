e008068ad8ad886ecefdf67e62ec1a9311d5aac5
Jared <jaredmcraig@gmail.com>
2015-10-15 15:08:37 -0600

    working

--- downloadAccelerator.py
+++ downloadAccelerator.py
@@ -12,7 +12,9 @@ class DownloaderAccelerator:
         self.threads = threads
         self.url = url
         self.rmin = 0
+        self.time = 0.0
         self.rmax = 1024
+        self.finished = False
         self.lock = threading.Lock()
         self.parse_options()
         self.run()
@@ -38,7 +40,6 @@ class DownloaderAccelerator:
         try:
             response = requests.head(self.url)
             self.length = int(response.headers.get('Content-Length'))
-            print response
             if (self.length <= 0):
                 raise IOError
             self.start()
@@ -51,6 +52,7 @@ class DownloaderAccelerator:
         print '%s(%d) bytes' % (local_filename, self.length)
         f = open(local_filename, 'wb')
         self.startThreads(f)
+        print 'url:%s threads:%d size:%d time:%f' % (self.url,self.threads,self.length,self.time)
         f.close()
         return local_filename
 
@@ -69,20 +71,31 @@ class DownloaderAccelerator:
     def download(self, i, f):
         while (True):
             self.lock.acquire()
+
             if (self.rmax == self.length):
-                self.lock.release()
-                break
-            if (self.rmax + 1024 < self.length):
-                self.rmin += 1024
-                self.rmax += 1024
-            else:
-                self.rmin += 1024
-                self.rmax = self.length
+                if (self.finished):
+                    self.lock.release()
+                    break
+                self.finished = True
+
             bytes = 'bytes=%d-%d' % (self.rmin, self.rmax)
             r = requests.get(self.url, headers={'Accept-Encoding': 'identity', 'Range': bytes}, stream=True)
-            print 'receiving %d bytes' % int(r.headers.get('Content-Length'))
-            #if r.content:
-                #f.write(r.content)
+            #print 'downloading range %d-%d out of %d total bytes' %(self.rmin,self.rmax,self.length)
+            #print 'Response code: %d' % (r.status_code)
+            #print 'thread %d\treceived %s bytes' % (i,r.headers.get('Content-Length'))
+            if (r.status_code == 200):
+                self.rmax = self.length
+                finished = True
+            if r.content:
+                f.write(r.content)
+
+            if (self.rmax + 1024 <= self.length):
+                self.rmin = self.rmax + 1
+                self.rmax += 1024
+            else:
+                self.rmin = self.rmax + 1
+                self.rmax = self.length
+
             self.lock.release()
 
 if __name__ == '__main__':
